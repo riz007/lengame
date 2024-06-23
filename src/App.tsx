9f7b48f60f22cfb2000e4e1b4686899a8f519987
riz007 <rizwanulrudra@gmail.com>
2024-06-23 15:22:54 +0700

    feat: create custom api hook to fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 function App() {
   return (
     <Grid
@@ -11,12 +12,11 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="orange.400">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg="blue.500">
-        Main
+      <GridItem area="main">
+        {" "}
+        <GameGrid />
       </GridItem>
     </Grid>
   );
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface IGame {
+  id: number;
+  name: string;
+}
+interface IGamesApiResponse {
+  count: number;
+  results: IGame[];
+}
+const useGames = () => {
+  const [games, setGames] = useState<IGame[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<IGamesApiResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+    return () => controller.abort();
+  }, []);
+  return { games, error };
+};
+export default useGames;
