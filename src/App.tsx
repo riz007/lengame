6ea3ef1189fa9ab6ab7b1a6a11a0bd0b084ce3fd
riz007 <rizwanulrudra@gmail.com>
2024-06-23 11:54:27 +0700

    feat: layout skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}>
+      <GridItem area="nav" bg="green.500">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="orange.400">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue.500">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
